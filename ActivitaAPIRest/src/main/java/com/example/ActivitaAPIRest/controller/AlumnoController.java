8e3824258bdf54da4ff2b5a4ed8556c95cd0cbf4
guillem <120484158+nugui1984@users.noreply.github.com>
2025-01-25 13:06:27 +0100

    Add files via upload
    Una altre versió alternativa

---
+++ ActivitaAPIRest/src/main/java/com/example/ActivitaAPIRest/controller/AlumnoController.java
@@ -0,0 +1,56 @@
+package com.example.ActivitaAPIRest.controller;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ActivitaAPIRest.model.AlumnoModel;
+import com.example.ActivitaAPIRest.service.AlumnoService;
+
+@RestController
+@RequestMapping("/alumno")
+public class AlumnoController {
+    private static final String Nombre = null;
+	@Autowired
+    AlumnoService alumnoService;
+
+    @GetMapping()
+    public ArrayList<AlumnoModel> obtenerUsuarios(){
+        return alumnoService.obtenerAlumnos();
+    }
+
+    @PostMapping()
+    public AlumnoModel guardarUsuario(@RequestBody AlumnoModel alumno){
+        return this.alumnoService.guardarAlumno(alumno);
+    }
+
+    @GetMapping( path = "/{id}")
+    public Optional<AlumnoModel> obtenerUsuarioPorId(@PathVariable("id") Long id) {
+        return this.alumnoService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<AlumnoModel> obtenerUsuarioPorNombre(@RequestParam("Nombre") String nombre){
+        return this.alumnoService.obtenerPorNombre(Nombre);
+    }
+
+    @DeleteMapping( path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.alumnoService.eliminarAlumno(id);
+        if (ok){
+            return "Se eliminó el alumno con id " + id;
+        }else{
+            return "No pudo eliminar el alumno con id" + id;
+        }
+    }
+
+}
---
+++ ActivitaAPIRest/src/main/java/com/example/ActivitaAPIRest/model/AlumnoModel.java
@@ -0,0 +1,46 @@
+package com.example.ActivitaAPIRest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="alumnos")
+public class AlumnoModel {
+
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 @Column(unique = true, nullable = false)
+	private Long id;
+	private String nombre;
+	private String email;
+	private int edad;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+}
